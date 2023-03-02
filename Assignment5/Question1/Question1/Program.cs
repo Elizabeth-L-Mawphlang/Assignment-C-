fe878759be66fa593960065a969bbc2be3092a44
Elizabeth-L-Mawphlang <72000872+Elizabeth-L-Mawphlang@users.noreply.github.com>
2023-03-02 10:48:44 +0530

    Add files via upload

---
+++ Assignment5/Question1/Question1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Undergraduate undergraduate = new Undergraduate();
+            undergraduate.Name = "Konsam Dev Kanta";
+            undergraduate.StudentId = 1224;
+            undergraduate.Grade = 30;
+            Console.WriteLine("Undergraduate is Passed: " + undergraduate.IsPassed(undergraduate.Grade));
+
+            Graduate graduate = new Graduate();
+            graduate.Name = "Jonah Suchiang";
+            graduate.StudentId = 45677;
+            graduate.Grade = 10;
+            Console.WriteLine("Graduate is Passed: " + graduate.IsPassed(graduate.Grade));
+            Console.Read();
+
+        }
+        public abstract class Student
+        {
+            public string Name { get; set; }
+            public int StudentId { get; set; }
+            public double Grade { get; set; }
+
+            public abstract bool IsPassed(double grade);
+        }
+
+        public class Undergraduate : Student
+        {
+            public override bool IsPassed(double grade)
+            {
+                if (grade > 70.0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public class Graduate : Student
+        {
+            public override bool IsPassed(double grade)
+            {
+                if (grade > 80.0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Assignment5/Question2/Question2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dayscholar d =new Dayscholar();
+            d.ShowDetails();
+            Console.WriteLine("\n***********************\n");
+            Resident r = new Resident();
+            r.ShowDetails();
+
+            Console.Read();
+        }
+        public interface IStudent
+        {
+            int StudentId { get; set; }
+            string Name { get; set; }
+            void ShowDetails();
+        }
+
+        public class Dayscholar : IStudent
+        {
+            public int StudentId { get; set; } = 123;
+            public string Name { get; set; } = "James";
+            public float Fees { get; set; } = 50000;
+
+            public void ShowDetails()
+            {
+                Console.WriteLine($"Student ID: {StudentId}\n Name: {Name}\n Fees: {Fees}" );
+            }
+        }
+
+        public class Resident : IStudent
+        {
+            public int StudentId { get; set; } = 321;
+            public string Name { get; set; } = "Alvin";
+            public float Fees { get; set; } = 100000;
+
+            public void ShowDetails()
+            {
+                Console.WriteLine($"Student ID: {StudentId}\n Name: {Name}\n Fees: { Fees}" );
+                Console.ReadLine();
+            }
+        }
+    }
+}

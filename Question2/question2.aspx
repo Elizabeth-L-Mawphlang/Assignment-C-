ba0186c1179260ac9a7b9508ea37eca0c830fc43
Elizabeth-L-Mawphlang <72000872+Elizabeth-L-Mawphlang@users.noreply.github.com>
2023-03-10 21:49:29 +0530

    Add files via upload

---
+++ Question2/question2.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="question2.aspx.cs" Inherits="question2.question2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" Height="54px" Width="83px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem Value="Tomatoes">Tomatoes</asp:ListItem>
+                <asp:ListItem Value="Potatoes">Potatoes</asp:ListItem>
+                <asp:ListItem Value="Chili">Chili</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Image ID="img" runat="server" Height="137px" Width="242px" />
+            <br />
+            <br />
+        </div>
+        <asp:Button ID="save" runat="server" Text="Button" Width="85px" OnClick="btn_click" />
+        <br />
+        <br />
+        <asp:Label ID="price" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Question2/question2.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace question2
+{
+    public partial class question2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedIndex == 0)
+            {
+                img.ImageUrl = "images/tomato.jfif";
+            }
+            else if (DropDownList1.SelectedIndex == 1)
+            {
+                img.ImageUrl = "images/potatoes.jfif";
+
+            }
+            else
+                img.ImageUrl = "images/chili.jfif";
+
+
+        }
+
+        protected void btn_click(object sender, EventArgs e)
+        {
+
+            if (DropDownList1.SelectedIndex > -1)
+            {
+                int cost = DropDownList1.SelectedIndex;
+                price.Text = "Rs ";
+
+                if (cost == 0)
+                    price.Text += "10";
+
+                else  if (cost == 1)
+                    price.Text += "100";
+
+
+
+                else 
+                    price.Text += "60";
+
+            }
+
+
+
+
+
+        }
+
+        
+    }
+}
